7ee2d3072b67e0e97aee53fa826ef1b0502c998a
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-09-20 16:12:50 +1000

    Modified background color, improve header animation and refractor some code

--- assets/js/main-header-scroll.js
+++ assets/js/main-header-scroll.js
@@ -1,16 +1,9 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-
-
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
-    document.getElementById("main-header").style.top = "0";
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("header").style.top = "0px";
   } else {
-    document.getElementById("main-header").style.top = "-160px";
+    document.getElementById("header").style.top = "-100px";
   }
 }
-
-
-// ACTIVE MENU LINKS
