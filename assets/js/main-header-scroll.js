782c49f3f1b56fd393f5e1dfe891759e86910f1d
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-27 14:45:39 +1100

    extended javascript file

--- assets/js/main-header-scroll.js
+++ assets/js/main-header-scroll.js
@@ -1,11 +1,24 @@
-function toggleNavBar() {
-  document.getElementById("navBar").classList.toggle("show");
+const navBarButton = document.querySelector("#navBarButton")
+
+console.log('navBarButton',': ', navBarButton);
+
+navBarButton.addEventListener('click',toggleNavBar)
+
+function toggleNavBar(e) {
+  e.preventDefault()
+  if(navBar.style.visibility == "hidden"){
+    navBar.style.visibility = "visible"
+  }else{
+    navBar.style.visibility = "hidden"
+  }
 }
 
 window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+function scrollFunction() {  
+  const headerTop =  document.documentElement.scrollTop
+  // console.log('headerTop',': ', headerTop);
+  const flag = 550
+  if (headerTop > flag) {
     document.getElementById("header").style.top = "0px";
   } else {
     document.getElementById("header").style.top = "-100px";
