14d6c396fbe33faf34b629b81796afb7ab80918f
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-03-18 09:28:11 +1100

    Relocated socials-section and adjusted spacing accordingly.

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Socials () {
   return (
-    <div className="section-below">
+    <div className="socials-section">
       <div className="socials">
         <a target="blank" href="https://github.com/DiegoRodriguez2018"><i className="fab fa-github-square"></i></a>
         <a target="blank" href="https://www.linkedin.com/in/diego-rodriguez-47226698/"><i className="fab fa-linkedin"></i></a>
--- src/Sections/Skillset.js
+++ src/Sections/Skillset.js
@@ -9,12 +9,14 @@ const languages = {
   ),
   SQLite: (
     <span>
-      <i className="devicons devicons-sqllite" />SQLite
+      <i className="devicons devicons-sqllite" />
+      SQLite
     </span>
   ),
   MongoDB: (
     <span>
-      <i className="devicons devicons-mongodb" />MongoDB
+      <i className="devicons devicons-mongodb" />
+      MongoDB
     </span>
   )
 };
@@ -24,63 +26,64 @@ export default function Skillset(props) {
     <section className="skills">
       <CloseButton resetDiegoRodriguez={props.resetDiegoRodriguez} />
       <h3>Skillset: </h3>
-      <div className="skillset">
-        <ul>
-          <li> {languages.PostgreSQL} </li>
-          <li> {languages.SQLite} </li>
-          <li> {languages.MongoDB} </li>
-        </ul>
-        <ul>
-          <li>
-            <i className="devicons devicons-github_badge" />Github
-          </li>
-          <li>
-            <i className="devicons devicons-python" />Python
-          </li>
-          <li>
-            <i className="devicons devicons-java" />Java
-          </li>
-        </ul>
-        <ul>
-          <li>
-            {" "}
-            <i className="devicons devicons-ruby" />Ruby
-          </li>
-          <li>
-            {" "}
-            <i className="devicons devicons-ruby_on_rails" />Ruby on Rails
-          </li>
-          <li>
-            <i className="devicons devicons-javascript" />Javascript
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <i className="devicons devicons-javascript" />React.js
-          </li>
-          <li>
-            <i className="devicons devicons-javascript" />Node.js{" "}
-          </li>
-          <li>
-            <i className="devicons devicons-javascript" />Express.js{" "}
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <i className="devicons devicons-javascript" />Vue.js{" "}
-          </li>
-          <li>
-            <i className="devicons devicons-css3" />CSS3/4{" "}
-          </li>
-          <li>
-            {" "}
-            <i className="devicons devicons-html5" />HTML5
-          </li>
-        </ul>
-      </div>
+      <ul className="skillset">
+        <li> {languages.PostgreSQL} </li>
+        <li> {languages.SQLite} </li>
+        <li> {languages.MongoDB} </li>
+
+        <li>
+          <i className="devicons devicons-github_badge" />
+          Github
+        </li>
+        <li>
+          <i className="devicons devicons-python" />
+          Python
+        </li>
+        <li>
+          <i className="devicons devicons-java" />
+          Java
+        </li>
+
+        <li>
+          <i className="devicons devicons-ruby" />
+          Ruby
+        </li>
+        <li>
+          <i className="devicons devicons-ruby_on_rails" />
+          Ruby on Rails
+        </li>
+        <li>
+          <i className="devicons devicons-javascript" />
+          Javascript
+        </li>
+
+        <li>
+          <i className="devicons devicons-javascript" />
+          React.js
+        </li>
+        <li>
+          <i className="devicons devicons-javascript" />
+          Node.js
+        </li>
+        <li>
+          <i className="devicons devicons-javascript" />
+          Express.js
+        </li>
+        <li>
+          <i className="devicons devicons-javascript" />
+          Vue.js
+        </li>
+        <li>
+          <i className="devicons devicons-css3" />
+          CSS3/4
+        </li>
+        <li>
+          <i className="devicons devicons-html5" />
+          HTML5
+        </li>
+      </ul>
       <br />
       <p>
-        {" "}
         I love coding and technology in general, and some of the areas I would
         like to expand in the future include; Data Science , Artificial
         Intelligence, Mobile Development and 3D Modelling.
